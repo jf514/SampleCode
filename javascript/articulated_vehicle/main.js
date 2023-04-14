9049e021024a93a830b68ca0e45a88a9725f67c8
Jason Foat <jasonfoat@Jasons-Laptop.local>
2023-04-14 11:46:57 -0700

    Updates to steering - use derivative

--- javascript/articulated_vehicle/main.js
+++ javascript/articulated_vehicle/main.js
@@ -12,7 +12,7 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-const size = 30;
+const size = 29;
 const divisions = 30;
 const gridHelper = new THREE.GridHelper( size, divisions );
 gridHelper.position.set(0, 0, 0);
@@ -24,43 +24,46 @@ camera.position.z = 20;
 var deltaT = .1;
 var t = 0.0;
 
+///////////////////////////////////////////////////////////////////
 class BicycleModel {
-	constructor(x, y, z, theta, delta, v, lf, lr) {
+	constructor(x, y, z, theta, steer, v, lf, lr) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 		this.theta = theta;
-		this.delta = delta;
+		this.steer = steer;
 		this.v = v;
 		this.lf = lf;
 		this.lr = lr; 
 	}
 
-	update(deltaT, delta){
-
-		// Slip angle
-		var beta = Math.atan(this.lr * Math.tan(delta)/(this.lf+this.lr))
-
+	update(deltaT, deltaSteer){
 		// Midpoint method - calc midpoints
+		var beta = Math.atan(this.lr * Math.tan(this.steer)/(this.lf+this.lr))  
+		var steer_mid = this.steer + (0.5*deltaT)*deltaSteer;
 		var x_mid = this.x + this.v*(0.5*deltaT)*Math.cos(this.theta + beta);
 		var y_mid = this.y + this.v*(0.5*deltaT)*Math.sin(this.theta + beta);
-		var theta_mid = this.theta + (0.5*deltaT)*(this.v/(this.lf + this.lr))*Math.tan(delta)*Math.cos(beta);
-
+		var theta_mid = this.theta + (0.5*deltaT)*(this.v/(this.lf + this.lr))*Math.tan(this.steer)*Math.cos(beta);
+		
 		// Full update
-		this.x = this.x + this.v*(deltaT)*Math.cos(theta_mid + beta);
-		this.y = this.y + this.v*(deltaT)*Math.sin(theta_mid + beta);
-		this.theta = this.theta + (deltaT)*(this.v/(this.lf + this.lr))*Math.tan(delta)*Math.cos(beta);
-
+		var beta_mid = Math.atan(this.lr * Math.tan(steer_mid)/(this.lf+this.lr))  
+		this.x = this.x + this.v*(deltaT)*Math.cos(theta_mid + beta_mid);
+		this.y = this.y + this.v*(deltaT)*Math.sin(theta_mid + beta_mid);
+		this.theta = this.theta + (deltaT)*(this.v/(this.lf + this.lr))*Math.tan(steer_mid)*Math.cos(beta_mid);
+		this.steer = this.steer + (deltaT)*deltaSteer;
 	}
 }
+///////////////////////////////////////////////////////////////////
 
-const bike = new BicycleModel(-15, -15, 0, 0.0, 0.0, 1.0, 1.5, 1.5);
+const bike = new BicycleModel(0, 14.5, 0.0, 0*0.25*Math.PI, -0.125*Math.PI, 0.5, 1.5, 1.5);
+///////////////////////////////////////////////////////////////////
 
 function animate() {
 	requestAnimationFrame( animate );
 
 	//cube.rotation.x += 0.01;
-	bike.update(deltaT, 0.125*Math.PI*Math.sin(2*Math.PI*t/(1000.0)));
+	var deltaSteer = 1.0*Math.PI*Math.sin(2*Math.PI*t/(100.0));
+	bike.update(deltaT, 0.0*deltaSteer);
 	cube.position.x = bike.x;
 	cube.position.y = bike.y;
 	cube.rotation.z = bike.theta;
