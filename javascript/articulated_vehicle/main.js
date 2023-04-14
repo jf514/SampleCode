e3ccc8a0bd90c303d2eb342b40bf545922b8cca4
Jason Foat <jasonfoat@Jasons-Laptop.local>
2023-04-14 10:30:53 -0700

    Bicycle model dynamics added.

--- javascript/articulated_vehicle/main.js
+++ javascript/articulated_vehicle/main.js
@@ -21,20 +21,53 @@ gridHelper.rotation.x = Math.PI/2;
 scene.add( gridHelper );
 
 camera.position.z = 20;
-var t = 0;
+var deltaT = .1;
+var t = 0.0;
+
+class BicycleModel {
+	constructor(x, y, z, theta, delta, v, lf, lr) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.theta = theta;
+		this.delta = delta;
+		this.v = v;
+		this.lf = lf;
+		this.lr = lr; 
+	}
+
+	update(deltaT, delta){
+
+		// Slip angle
+		var beta = Math.atan(this.lr * Math.tan(delta)/(this.lf+this.lr))
+
+		// Midpoint method - calc midpoints
+		var x_mid = this.x + this.v*(0.5*deltaT)*Math.cos(this.theta + beta);
+		var y_mid = this.y + this.v*(0.5*deltaT)*Math.sin(this.theta + beta);
+		var theta_mid = this.theta + (0.5*deltaT)*(this.v/(this.lf + this.lr))*Math.tan(delta)*Math.cos(beta);
+
+		// Full update
+		this.x = this.x + this.v*(deltaT)*Math.cos(theta_mid + beta);
+		this.y = this.y + this.v*(deltaT)*Math.sin(theta_mid + beta);
+		this.theta = this.theta + (deltaT)*(this.v/(this.lf + this.lr))*Math.tan(delta)*Math.cos(beta);
+
+	}
+}
+
+const bike = new BicycleModel(-15, -15, 0, 0.0, 0.0, 1.0, 1.5, 1.5);
 
 function animate() {
 	requestAnimationFrame( animate );
 
 	//cube.rotation.x += 0.01;
-	cube.rotation.z += 0.01;
+	bike.update(deltaT, 0.125*Math.PI*Math.sin(2*Math.PI*t/(1000.0)));
+	cube.position.x = bike.x;
+	cube.position.y = bike.y;
+	cube.rotation.z = bike.theta;
 
-	//gridHelper.rotation.x += 0.01;
+	t += deltaT;
 
-	t += 0.01;
-
-	cube.position.z = Math.sin(t);
-	
+	//gridHelper.rotation.x += 0.01;	
 	renderer.render( scene, camera );
 }
 
